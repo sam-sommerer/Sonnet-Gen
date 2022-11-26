8ab9bc25a43b9723be7c63f357a2f96a4dff543e
sam-sommerer <sommerer@usc.edu>
2022-11-26 13:54:55 -0800

    add print

--- keyword/generate_rhymes.py
+++ keyword/generate_rhymes.py
@@ -19,6 +19,8 @@ def generate_rhymes(model, keywords, initial_rhyming_lines, countin_rhyming_line
         candidates = get_rhyme_candidates(word, N=30)
         # candidates = pronouncing.rhymes(word)
 
+        temp = keywords.split(" Keywords")[countin_rhyming_lines[i]].split("'")
+        print(f"temp: {temp}")
         replace_word = keywords.split(" Keywords")[countin_rhyming_lines[i]].split("'")[5]
         print(f"replace_word: {replace_word}")
 
