a4002fe5b692b52e4118b88ea255c5de31942387
sam-sommerer <sommerer@usc.edu>
2022-12-13 01:12:57 -0800

    add polishing cmd line arg to poem gen and update poem slurm script

--- generate_poem.py
+++ generate_poem.py
@@ -14,19 +14,24 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--title", type=str)
+    parser.add_argument("--polish", action="store_true")
     args = parser.parse_args()
 
     raw_keywords = inference_bart_keywords_gen.get_keywords(title=args.title)
     rhyming_keywords = generate_rhymes.get_rhymes(keywords=raw_keywords)
 
-    polished_keywords = aesthetics.imagery_replacement(keywords=rhyming_keywords)
-    poem = decode.get_poem(title=args.title, keywords=polished_keywords)
-    # poem = decode.get_poem(title=args.title, keywords=rhyming_keywords)
+    poem = ""
+
+    if args.polish:
+        polished_keywords = aesthetics.imagery_replacement(keywords=rhyming_keywords)
+        poem = decode.get_poem(title=args.title, keywords=polished_keywords)
+    else:
+        poem = decode.get_poem(title=args.title, keywords=rhyming_keywords)
 
     print(f"final poem: {poem}")
 
     output_dir = "generated_poems/"
-    output_filename = args.title + ".txt"
+    output_filename = args.title + ".txt" if not args.polish else args.title + "_polish.txt"
     output_path = (output_dir + output_filename).replace(" ", "_")
 
     with open(output_path, "w+") as f:
